b960076f788a905e00c27af07b9b45a24c37146a
Kevin Andrew Kotian <kevinandrew.k@gmail.com>
2024-01-16 03:16:17 +0530

    shifted to leaflet

--- osm_website/__manifest__.py
+++ osm_website/__manifest__.py
@@ -14,13 +14,18 @@
             # '/osm_website/static/src/js/OpenLayers.js',
             '/osm_website/static/src/js/snippet.js',
             # '/osm_website/static/src/js/options.js',
+            '/osm_website/static/src/js/hello.js',
             '/osm_website/static/src/css/snippet.css',
             '/osm_website/views/myStyle.css',
             # 'templates/assets.xml',
+            '/osm_website/static/src/js/leaflet.js',
+            '/osm_website/static/src/js/test_leaflet.js',
+            '/osm_website/static/src/css/leaflet.css',
+            '/osm_website/static/src/css/test_leaflet.css'
         ],
-        'web.assets_backend': [
-            '/osm_website/static/src/js/hello.js',
-        ],
+        # 'web.assets_backend': [
+        #     '/osm_website/static/src/js/hello.js',
+        # ],
         'web.assets_qweb': [
             # '/osm_website/static/src/xml/snippet.xml',
             'templates/assets.xml',
