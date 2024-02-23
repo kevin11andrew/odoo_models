dfcfb06493e9f7c0b15a916b8b0c25deb647b4c7
R0-00 <rithwik@fsmk.org>
2024-02-23 12:58:09 +0530

    test changes

--- osm_website/__manifest__.py
+++ osm_website/__manifest__.py
@@ -4,23 +4,37 @@
     'version': '1.0.0',
     'category': 'osmManager',
     'description': 'Allows us to create and edit osm building block in website module',
-    'depends': ['website'],
+    'depends': [
+        'digest',
+        'web',
+        'web_editor',
+        'http_routing',
+        'portal',
+        'social_media',
+        'auth_signup',
+        'mail',
+        'google_recaptcha',
+        'utm',
+    ],
     'data': [
         'views/snippets/snippet.xml',
         # 'views/snippets/options.xml'
     ],
     'assets': {
         'web.assets_frontend': [
-            '/osm_website/static/src/js/snippet.js',
-            '/osm_website/static/src/js/hello.js',
+            '/osm_website/static/src/js/options.js',
+            'osm_website/static/src/js/000.js',
+            'osm_website/static/src/js/000.scss',
+            #'/osm_website/static/src/js/snippet.js',
+            #'/osm_website/static/src/js/hello.js',
             # 'templates/assets.xml',
-            '/osm_website/static/src/js/leaflet.js',
-            '/osm_website/static/src/js/test_leaflet.js',
-            '/osm_website/static/src/css/leaflet.css',
-            '/osm_website/static/src/css/test_leaflet.css',
+            #'/osm_website/static/src/js/leaflet.js',
+            #'/osm_website/static/src/js/test_leaflet.js',
+            #'/osm_website/static/src/css/leaflet.css',
+            #'/osm_website/static/src/css/test_leaflet.css',
 
-            '/osm_website/static/src/js/test1.js',
-            '/osm_website/static/src/js/test1.js'
+            #'/osm_website/static/src/js/test1.js',
+            #'/osm_website/static/src/js/test1.js'
         ],
         # 'web.assets_backend': [
         #     '/osm_website/static/src/js/hello.js',
--- osm_website/static/src/js/options.js
+++ osm_website/static/src/js/options.js
@@ -1,19 +1,115 @@
 /** @odoo-module **/
-import  { renderToElement } from "@web/core/utils/render";
-import publicWidget from "@web/legacy/js/public/public_widget" ;
-const OSMSnippet = publicWidget.Widget.extend({
-    selector: ".s_map_osm",
-    // start()
-    onBuilt()
-    {
-        console.log("HELLO");
-        this.containerEl=this.$target[0].querySelector(".s_map_color_filter");
-        this.containerEl.appendChild(renderToElement("osm_website.map_data",{
-            widget:this
-        })
-        );
+
+import {_t} from 'web.core';
+import options from 'web_editor.snippets.options';
+import {generateGMapIframe, generateGMapLink} from 'website.utils';
+
+
+console.log("Loaded Options.js")
+options.registry.Osmmap = options.Class.extend({
+    /**
+     * @override
+     */
+    onBuilt() {
+        // The iframe is added here to the snippet when it is dropped onto the
+        // page. However, in the case where a custom snippet saved by the user
+        // is dropped, the iframe already exists and doesn't need to be added
+        // again.
+        console.log("BUILD MAP IN PROGRESS")
+        if (!this.$target[0].querySelector('.s_map_embedded')) {
+            const iframeEl = generateGMapIframe();
+            this.$target[0].querySelector('.s_map_osm_color_filter').before(iframeEl);
+            this._updateSource();
+        }
     },
-    destroy() { this.containerEl.remove() }
+
+    //--------------------------------------------------------------------------
+    // Options
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see this.selectClass for parameters
+     */
+    async selectDataAttribute(previewMode, widgetValue, params) {
+        await this._super(...arguments);
+        if (['mapAddress', 'mapType', 'mapZoom'].includes(params.attributeName)) {
+            this._updateSource();
+        }
+    },
+    /**
+     * @see this.selectClass for parameters
+     */
+    async showDescription(previewMode, widgetValue, params) {
+        const descriptionEl = this.$target[0].querySelector('.description');
+        if (widgetValue && !descriptionEl) {
+            this.$target.append($(`
+                <div class="description">
+                    <font>${_t('Visit us:')}</font>
+                    <span>${_t('Our office is open Monday – Friday 8:30 a.m. – 4:00 p.m.')}</span>
+                </div>`)
+            );
+        } else if (!widgetValue && descriptionEl) {
+            descriptionEl.remove();
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _computeWidgetState(methodName, params) {
+        if (methodName === 'showDescription') {
+            return !!this.$target[0].querySelector('.description');
+        }
+        return this._super(...arguments);
+    },
+    /**
+     * @private
+     */
+    _updateSource() {
+        const dataset = this.$target[0].dataset;
+        const $embedded = this.$target.find('.s_map_embedded');
+        const $info = this.$target.find('.missing_option_warning');
+        if (dataset.mapAddress) {
+            const url = generateGMapLink(dataset);
+            if (url !== $embedded.attr('src')) {
+                $embedded.attr('src', url);
+            }
+            $embedded.removeClass('d-none');
+            $info.addClass('d-none');
+        } else {
+            $embedded.attr('src', 'about:blank');
+            $embedded.addClass('d-none');
+            $info.removeClass('d-none');
+        }
+    },
 });
 
-publicWidget.registry.s_map_osm = OSMSnippet;
+export default {
+    Map: options.registry.Osmmap,
+};
+
+
+
+///** @odoo-module **/
+//import  { renderToElement } from "@web/core/utils/render";
+//import publicWidget from "@web/legacy/js/public/public_widget" ;
+//const OSMSnippet = publicWidget.Widget.extend({
+//    selector: ".s_map_osm",
+//    // start()
+//    onBuilt()
+//    {
+//        console.log("HELLO");
+//        this.containerEl=this.$target[0].querySelector(".s_map_color_filter");
+//        this.containerEl.appendChild(renderToElement("osm_website.map_data",{
+//            widget:this
+//        })
+//        );
+//    },
+//    destroy() { this.containerEl.remove() }
+//});
+//
+//publicWidget.registry.s_map_osm = OSMSnippet;
