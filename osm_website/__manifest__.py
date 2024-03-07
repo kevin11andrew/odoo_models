2b4352b746b19e277867de22201223dbfde0c138
Kevin Andrew Kotian <kevinandrew.k@gmail.com>
2024-03-07 14:39:43 +0530

    Map is being rendered in the edit window

--- osm_website/__manifest__.py
+++ osm_website/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'osm Manager',
+    'name': 'osm Manager for Website',
     'author': 'Kevin',
     'version': '1.0.0',
     'category': 'osmManager',
     'description': 'Allows us to create and edit osm building block in website module',
     'depends': [
+        'website',
         'digest',
         'web',
         'web_editor',
@@ -22,16 +23,18 @@
     ],
     'assets': {
         'web.assets_frontend': [
+            '/osm_website/static/src/css/leaflet.css',
+            '/osm_website/static/src/css/test_leaflet.css',
             '/osm_website/static/src/js/options.js',
             'osm_website/static/src/js/000.js',
             'osm_website/static/src/js/000.scss',
+            '/osm_website/static/src/js/leaflet.js',
             #'/osm_website/static/src/js/snippet.js',
             #'/osm_website/static/src/js/hello.js',
             # 'templates/assets.xml',
             #'/osm_website/static/src/js/leaflet.js',
             #'/osm_website/static/src/js/test_leaflet.js',
-            #'/osm_website/static/src/css/leaflet.css',
-            #'/osm_website/static/src/css/test_leaflet.css',
+
 
             #'/osm_website/static/src/js/test1.js',
             #'/osm_website/static/src/js/test1.js'
--- osm_website/static/src/js/000.js
+++ osm_website/static/src/js/000.js
@@ -2,6 +2,7 @@
 
 import publicWidget from 'web.public.widget';
 import {generateGMapLink, generateGMapIframe} from 'website.utils';
+// import leaflet from 'osm_website'
 
 console.log("Loaded 000.js")
 
@@ -19,11 +20,35 @@ publicWidget.registry.Osmmap = publicWidget.Widget.extend({
             // description field.
             // In such cases, reconstruct the iframe.
             const dataset = this.el.dataset;
-            if (dataset.mapAddress) {
-                const iframeEl = generateGMapIframe();
-                iframeEl.setAttribute('src', generateGMapLink(dataset));
-                this.el.querySelector('.s_map_osm_color_filter').before(iframeEl);
-            }
+            // if (dataset.mapAddress) {
+            //     const iframeEl = generateGMapIframe();
+            //     iframeEl.setAttribute('src', generateGMapLink(dataset));
+            //     this.el.querySelector('.s_map_osm_color_filter').before(iframeEl);
+            // }
+
+            console.log("dataset.mapAddress")
+            console.log(dataset.mapAddress)
+            var map = L.map('s_map_frame').setView([51.505, -0.09], 14);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+            // place=prompt("Place")
+            fetch("https://nominatim.openstreetmap.org/search?format=json&q="+dataset)
+                .then(result => result.json())
+                .then(parsedResult => {
+                    console.log(parsedResult.length);
+                    console.log(parsedResult)
+                    // console.log(parsedResult[0].display_name)
+                    if(parsedResult.length>0)
+                    {
+                        var marker = L.marker([parsedResult[0].lat, parsedResult[0].lon]).addTo(map);parsedResult[0].lat
+                        map.setView(new L.LatLng(parsedResult[0].lat, parsedResult[0].lon),14)
+                    }
+                    else
+                        console.log("Not Found")
+                });
+
         }
         return this._super(...arguments);
     },
